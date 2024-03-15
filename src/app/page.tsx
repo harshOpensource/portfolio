323dae92033e6dcab994632a6cd276b9d0c168b4
harshOpensource <harshopensource@gmail.com>
2024-03-15 19:24:20 +0530

    final version 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { About } from "@/components/about-me";
+import Education from "@/components/education";
+import Experience from "@/components/experience";
 import { Hero } from "@/components/hero";
 import { SelectedProjects } from "@/components/projects";
 import { projectsData } from "@/config/projects";
@@ -9,6 +11,14 @@ export default function Home() {
       <Hero />
       <About />
       <SelectedProjects projects={projectsData} />
+      <div className="text-center font-title text-3xl font-bold sm:text-4xl mb-16">
+        Experience
+      </div>
+      <Experience />
+      <div className="text-center font-title text-3xl font-bold sm:text-4xl mb-16 mt-28">
+        Education
+      </div>
+      <Education />
     </>
   );
 }
